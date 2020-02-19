53bb93eef688161d5663cefa0f32cd8986d59e96
Jhavaa <Jramisetty@ufl.edu>
2020-02-19 12:48:52 -0500

    Need to understand how to handle onSubmit

--- app/components/LoginForm/index.js
+++ app/components/LoginForm/index.js
@@ -4,26 +4,57 @@
  *
  */
 
-import React, { memo } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 // import styled from 'styled-components';
 
 import Form from './Form';
 import StyledInput from './StyledInput';
+import Button from '../Button/index.js';
 
-const handleClick = () => {};
+// constructor(props){
+//   super(props);
+//   this.state = {username: '', pass: ''};
+
+//   this.handleChange = this.handleChange.bind(this);
+//   this.handleSubmit = this.handleSubmit.bind(this);
+// }
+
+// handleChange(event){
+//   this.setState({value: event.target.value});
+// }
+
+const handleSubmit = () => {
+  console.log("Hello?")
+  fetch('http://localhost:8080/api/auth/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      username: username,
+      pass: pass,
+    })
+  })
+};
 
 function LoginForm() {
   return (
     <Form>
       <h2>Login</h2>
-      <StyledInput type="text" placeholder="email" />
-      <StyledInput type="password" placeholder="password" />
-      <button onClick={handleClick}>Login</button>
+      {/* This doesn't work because "state" is unrecognized
+      <StyledInput type="text" value={this.state.value} onChange={this.handleChange} placeholder="Username" />
+      <StyledInput type="password" value={this.state.value} onChange={this.handleChange} placeholder="Password" /> */}
+      
+      {/* This doesn't work because there is nothing handling the input */}
+      
+      <StyledInput type="text" placeholder="Username" />
+      <StyledInput type="password" placeholder="Password" />
+      <Button onClick={handleSubmit} text="Login" />
     </Form>
   );
 }
 
 LoginForm.propTypes = {};
 
-export default memo(LoginForm);
+export default LoginForm;
