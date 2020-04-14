695e267bbb7a5eaf9d3dc98ee0a2387ff8fd873c
Evan Ristow <evan.ristow1@gmail.com>
2020-04-14 12:43:12 -0400

    ESLint CI fix.

--- app/containers/Register/index.js
+++ app/containers/Register/index.js
@@ -146,7 +146,7 @@ export function Register({
   );
 }
 
-Register.prop = {
+Register.propTypes = {
   // dispatch: PropTypes.func.isRequired,
   email: PropTypes.string,
   setEmail: PropTypes.func,
